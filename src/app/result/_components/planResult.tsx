ab88cb8a6fb74f169e232e68301abc1a4573f112
itsmiraz <mirajulislamofficial044@gmail.com>
2024-10-27 19:15:36 +0600

    result page started

--- src/app/result/_components/planResult.tsx
+++ src/app/result/_components/planResult.tsx
@@ -1,9 +1,59 @@
-import React from 'react'
+"use client";
+import React, { useState } from "react";
 
 const PlanResult = () => {
-  return (
-    <div>PlanResult</div>
-  )
-}
+  const [selectedDate, setSelectedDate] = useState("");
 
-export default PlanResult
+  const Dates = [
+    {
+      title: "Day 1-2",
+      id: Math.random().toString(),
+    },
+    {
+      title: "Day 3-5",
+      id: Math.random().toString(),
+    },
+    {
+      title: "Day 6-7",
+      id: Math.random().toString(),
+    },
+    {
+      title: "Day 8-10",
+      id: Math.random().toString(),
+    },
+    {
+      title: "Day 11-14",
+      id: Math.random().toString(),
+    },
+  ];
+
+  return (
+    <div>
+      {/* Dates */}
+      <div className="flex justify-center gap-x-[20px] items-center">
+        {Dates.map((item) => (
+          <button
+            key={item.id}
+            className={`${
+              selectedDate === item.title
+                ? "bg-[#14591D] text-[#FDF7FA] font-bold border-none"
+                : "text-[#14591D] border"
+            } transition-all ease-in-out duration-300 px-[20px] leading-[14px] font-inter h-fit py-[18px]  border-[#14591D]/15 rounded-[50px] `}
+            onClick={() => setSelectedDate(item.title)}
+          >
+            {item.title} 
+          </button>
+        ))}
+      </div>
+
+      {/* result */}
+
+      <div className="bg-[#0924AB]/5 p-[30px] ">
+      
+      
+      </div>
+    </div>
+  );
+};
+
+export default PlanResult;
